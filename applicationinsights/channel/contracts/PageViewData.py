047ca38f072667d6c90db71c3355fc523424c390
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2019-02-01 17:08:31 -0800

    add context to PageViewData

--- applicationinsights/channel/contracts/PageViewData.py
+++ applicationinsights/channel/contracts/PageViewData.py
@@ -1,14 +1,18 @@
 import collections
 import copy
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from TelemetryContext import TelemetryContext
 from .Utils import _write_complex_object
 
 class PageViewData(object):
     """Data contract class for type PageViewData.
     """
-    ENVELOPE_TYPE_NAME = 'Microsoft.ApplicationInsights.PageView'	
-    	
+    ENVELOPE_TYPE_NAME = 'Microsoft.ApplicationInsights.PageView'
+
     DATA_TYPE_NAME = 'PageViewData'
-    
+
     _defaults = collections.OrderedDict([
         ('ver', 2),
         ('url', None),
@@ -19,7 +23,7 @@ class PageViewData(object):
         ('properties', {}),
         ('measurements', {})
     ])
-    
+
     def __init__(self):
         """Initializes a new instance of the class.
         """
@@ -27,41 +31,42 @@ class PageViewData(object):
             'ver': 2,
             'name': None,
         }
+        self._context = TelemetryContext()
         self._initialize()
-        
+
     @property
     def ver(self):
         """The ver property.
-        
+
         Returns:
             (int). the property value. (defaults to: 2)
         """
         return self._values['ver']
-        
+
     @ver.setter
     def ver(self, value):
         """The ver property.
-        
+
         Args:
             value (int). the property value.
         """
         self._values['ver'] = value
-        
+
     @property
     def url(self):
         """The url property.
-        
+
         Returns:
             (string). the property value. (defaults to: None)
         """
         if 'url' in self._values:
             return self._values['url']
         return self._defaults['url']
-        
+
     @url.setter
     def url(self, value):
         """The url property.
-        
+
         Args:
             value (string). the property value.
         """
@@ -69,40 +74,40 @@ class PageViewData(object):
             del self._values['url']
         else:
             self._values['url'] = value
-        
+
     @property
     def name(self):
         """The name property.
-        
+
         Returns:
             (string). the property value. (defaults to: None)
         """
         return self._values['name']
-        
+
     @name.setter
     def name(self, value):
         """The name property.
-        
+
         Args:
             value (string). the property value.
         """
         self._values['name'] = value
-        
+
     @property
     def duration(self):
         """The duration property.
-        
+
         Returns:
             (string). the property value. (defaults to: None)
         """
         if 'duration' in self._values:
             return self._values['duration']
         return self._defaults['duration']
-        
+
     @duration.setter
     def duration(self, value):
         """The duration property.
-        
+
         Args:
             value (string). the property value.
         """
@@ -110,22 +115,22 @@ class PageViewData(object):
             del self._values['duration']
         else:
             self._values['duration'] = value
-        
+
     @property
     def id(self):
         """The id property.
-        
+
         Returns:
             (string). the property value. (defaults to: None)
         """
         if 'id' in self._values:
             return self._values['id']
         return self._defaults['id']
-        
+
     @id.setter
     def id(self, value):
         """The id property.
-        
+
         Args:
             value (string). the property value.
         """
@@ -133,22 +138,22 @@ class PageViewData(object):
             del self._values['id']
         else:
             self._values['id'] = value
-        
+
     @property
     def referrer_uri(self):
         """The referrer_uri property.
-        
+
         Returns:
             (string). the property value. (defaults to: None)
         """
         if 'referrerUri' in self._values:
             return self._values['referrerUri']
         return self._defaults['referrerUri']
-        
+
     @referrer_uri.setter
     def referrer_uri(self, value):
         """The referrer_uri property.
-        
+
         Args:
             value (string). the property value.
         """
@@ -156,11 +161,11 @@ class PageViewData(object):
             del self._values['referrerUri']
         else:
             self._values['referrerUri'] = value
-        
+
     @property
     def properties(self):
         """The properties property.
-        
+
         Returns:
             (hash). the property value. (defaults to: {})
         """
@@ -168,11 +173,11 @@ class PageViewData(object):
             return self._values['properties']
         self._values['properties'] = copy.deepcopy(self._defaults['properties'])
         return self._values['properties']
-        
+
     @properties.setter
     def properties(self, value):
         """The properties property.
-        
+
         Args:
             value (hash). the property value.
         """
@@ -180,11 +185,11 @@ class PageViewData(object):
             del self._values['properties']
         else:
             self._values['properties'] = value
-        
+
     @property
     def measurements(self):
         """The measurements property.
-        
+
         Returns:
             (hash). the property value. (defaults to: {})
         """
@@ -192,11 +197,11 @@ class PageViewData(object):
             return self._values['measurements']
         self._values['measurements'] = copy.deepcopy(self._defaults['measurements'])
         return self._values['measurements']
-        
+
     @measurements.setter
     def measurements(self, value):
         """The measurements property.
-        
+
         Args:
             value (hash). the property value.
         """
@@ -204,15 +209,24 @@ class PageViewData(object):
             del self._values['measurements']
         else:
             self._values['measurements'] = value
-        
+
+    @property
+    def context(self):
+        """The context property.
+
+        Returns:
+            (object). the property value.
+        """
+        return self._context
+
     def _initialize(self):
         """Initializes the current instance of the object.
         """
         pass
-    
+
     def write(self):
         """Writes the contents of this object and returns the content as a dict object.
-        
+
         Returns:
             (dict). the object that represents the same data as the current instance.
         """
--- tests/applicationinsights_tests/channel_tests/contracts_tests/TestPageViewData.py
+++ tests/applicationinsights_tests/channel_tests/contracts_tests/TestPageViewData.py
@@ -16,7 +16,7 @@ class TestPageViewData(unittest.TestCase):
     def test_construct(self):
         item = PageViewData()
         self.assertNotEqual(item, None)
-    
+
     def test_ver_property_works_as_expected(self):
         expected = 42
         item = PageViewData()
@@ -27,7 +27,7 @@ class TestPageViewData(unittest.TestCase):
         item.ver = expected
         actual = item.ver
         self.assertEqual(expected, actual)
-    
+
     def test_url_property_works_as_expected(self):
         expected = 'Test string'
         item = PageViewData()
@@ -38,7 +38,7 @@ class TestPageViewData(unittest.TestCase):
         item.url = expected
         actual = item.url
         self.assertEqual(expected, actual)
-    
+
     def test_name_property_works_as_expected(self):
         expected = 'Test string'
         item = PageViewData()
@@ -49,7 +49,7 @@ class TestPageViewData(unittest.TestCase):
         item.name = expected
         actual = item.name
         self.assertEqual(expected, actual)
-    
+
     def test_duration_property_works_as_expected(self):
         expected = 'Test string'
         item = PageViewData()
@@ -60,17 +60,17 @@ class TestPageViewData(unittest.TestCase):
         item.duration = expected
         actual = item.duration
         self.assertEqual(expected, actual)
-    
+
     def test_properties_property_works_as_expected(self):
         item = PageViewData()
         actual = item.properties
         self.assertNotEqual(actual, None)
-    
+
     def test_measurements_property_works_as_expected(self):
         item = PageViewData()
         actual = item.measurements
         self.assertNotEqual(actual, None)
-    
+
     def test_serialize_works_as_expected(self):
         item = PageViewData()
         item.ver = 42
@@ -85,3 +85,7 @@ class TestPageViewData(unittest.TestCase):
         expected = '{"ver":42,"url":"Test string","name":"Test string","duration":"Test string","properties":{"key1":"test value 1","key2":"test value 2"},"measurements":{"key1":3.1415,"key2":42.2}}'
         self.assertEqual(expected, actual)
 
+    def test_context_property_works_as_expected(self):
+        item = PageViewData()
+        actual = item._context
+        self.assertNotEqual(None, actual)
