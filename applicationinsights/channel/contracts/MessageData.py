131780a34d40f1d390868c6dd2a0e0eb9f915ccc
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2019-02-01 17:05:21 -0800

    add context to MessageData

--- applicationinsights/channel/contracts/MessageData.py
+++ applicationinsights/channel/contracts/MessageData.py
@@ -1,36 +1,40 @@
 import collections
 import copy
 import logging
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from TelemetryContext import TelemetryContext
 from .Utils import _write_complex_object
 
 class MessageData(object):
     """Data contract class for type MessageData.
     """
 
-    ENVELOPE_TYPE_NAME = 'Microsoft.ApplicationInsights.Message'	
-    	
-    DATA_TYPE_NAME = 'MessageData'	
-    	
-    PYTHON_LOGGING_LEVELS = {	
-        'DEBUG': 0,	
-        'INFO': 1,	
-        'WARNING': 2,	
-        'ERROR': 3,	
-        'CRITICAL': 4,	
-        logging.DEBUG: 0,	
-        logging.INFO: 1,	
-        logging.WARNING: 2,	
-        logging.ERROR: 3,	
-        logging.CRITICAL: 4	
-    }	
-    
+    ENVELOPE_TYPE_NAME = 'Microsoft.ApplicationInsights.Message'
+
+    DATA_TYPE_NAME = 'MessageData'
+
+    PYTHON_LOGGING_LEVELS = {
+        'DEBUG': 0,
+        'INFO': 1,
+        'WARNING': 2,
+        'ERROR': 3,
+        'CRITICAL': 4,
+        logging.DEBUG: 0,
+        logging.INFO: 1,
+        logging.WARNING: 2,
+        logging.ERROR: 3,
+        logging.CRITICAL: 4
+    }
+
     _defaults = collections.OrderedDict([
         ('ver', 2),
         ('message', None),
         ('severityLevel', None),
         ('properties', {})
     ])
-    
+
     def __init__(self):
         """Initializes a new instance of the class.
         """
@@ -38,59 +42,60 @@ class MessageData(object):
             'ver': 2,
             'message': None,
         }
+        self._context = TelemetryContext()
         self._initialize()
-        
+
     @property
     def ver(self):
         """The ver property.
-        
+
         Returns:
             (int). the property value. (defaults to: 2)
         """
         return self._values['ver']
-        
+
     @ver.setter
     def ver(self, value):
         """The ver property.
-        
+
         Args:
             value (int). the property value.
         """
         self._values['ver'] = value
-        
+
     @property
     def message(self):
         """The message property.
-        
+
         Returns:
             (string). the property value. (defaults to: None)
         """
         return self._values['message']
-        
+
     @message.setter
     def message(self, value):
         """The message property.
-        
+
         Args:
             value (string). the property value.
         """
         self._values['message'] = value
-        
+
     @property
     def severity_level(self):
         """The severity_level property.
-        
+
         Returns:
             (int). the property value. (defaults to: None)
         """
         if 'severityLevel' in self._values:
             return self._values['severityLevel']
         return self._defaults['severityLevel']
-        
+
     @severity_level.setter
     def severity_level(self, value):
         """The severity_level property.
-        
+
         Args:
             value (int). the property value.
         """
@@ -98,11 +103,11 @@ class MessageData(object):
             del self._values['severityLevel']
         else:
             self._values['severityLevel'] = value
-        
+
     @property
     def properties(self):
         """The properties property.
-        
+
         Returns:
             (hash). the property value. (defaults to: {})
         """
@@ -110,11 +115,11 @@ class MessageData(object):
             return self._values['properties']
         self._values['properties'] = copy.deepcopy(self._defaults['properties'])
         return self._values['properties']
-        
+
     @properties.setter
     def properties(self, value):
         """The properties property.
-        
+
         Args:
             value (hash). the property value.
         """
@@ -122,15 +127,24 @@ class MessageData(object):
             del self._values['properties']
         else:
             self._values['properties'] = value
-        
+
+    @property
+    def context(self):
+        """The context property.
+
+        Returns:
+            (object). the property value.
+        """
+        return self._context
+
     def _initialize(self):
         """Initializes the current instance of the object.
         """
         pass
-    
+
     def write(self):
         """Writes the contents of this object and returns the content as a dict object.
-        
+
         Returns:
             (dict). the object that represents the same data as the current instance.
         """
--- tests/applicationinsights_tests/channel_tests/contracts_tests/TestMessageData.py
+++ tests/applicationinsights_tests/channel_tests/contracts_tests/TestMessageData.py
@@ -16,7 +16,7 @@ class TestMessageData(unittest.TestCase):
     def test_construct(self):
         item = MessageData()
         self.assertNotEqual(item, None)
-    
+
     def test_ver_property_works_as_expected(self):
         expected = 42
         item = MessageData()
@@ -27,7 +27,7 @@ class TestMessageData(unittest.TestCase):
         item.ver = expected
         actual = item.ver
         self.assertEqual(expected, actual)
-    
+
     def test_message_property_works_as_expected(self):
         expected = 'Test string'
         item = MessageData()
@@ -38,7 +38,7 @@ class TestMessageData(unittest.TestCase):
         item.message = expected
         actual = item.message
         self.assertEqual(expected, actual)
-    
+
     def test_severity_level_property_works_as_expected(self):
         expected = object()
         item = MessageData()
@@ -49,12 +49,12 @@ class TestMessageData(unittest.TestCase):
         item.severity_level = expected
         actual = item.severity_level
         self.assertEqual(expected, actual)
-    
+
     def test_properties_property_works_as_expected(self):
         item = MessageData()
         actual = item.properties
         self.assertNotEqual(actual, None)
-    
+
     def test_serialize_works_as_expected(self):
         item = MessageData()
         item.ver = 42
@@ -66,3 +66,7 @@ class TestMessageData(unittest.TestCase):
         expected = '{"ver":42,"message":"Test string","severityLevel":{},"properties":{"key1":"test value 1","key2":"test value 2"}}'
         self.assertEqual(expected, actual)
 
+    def test_context_property_works_as_expected(self):
+        item = MessageData()
+        actual = item._context
+        self.assertNotEqual(None, actual)
