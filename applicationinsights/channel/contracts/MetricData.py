b49987d6940e9c8d0b5adef95795322ddbda925e
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2019-02-01 17:06:52 -0800

    add context to MetricData

--- applicationinsights/channel/contracts/MetricData.py
+++ applicationinsights/channel/contracts/MetricData.py
@@ -1,21 +1,25 @@
 import collections
 import copy
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from TelemetryContext import TelemetryContext
 from .Utils import _write_complex_object
 
 class MetricData(object):
     """Data contract class for type MetricData.
     """
 
-    ENVELOPE_TYPE_NAME = 'Microsoft.ApplicationInsights.Metric'	
-    	
-    DATA_TYPE_NAME = 'MetricData'	
+    ENVELOPE_TYPE_NAME = 'Microsoft.ApplicationInsights.Metric'
+
+    DATA_TYPE_NAME = 'MetricData'
 
     _defaults = collections.OrderedDict([
         ('ver', 2),
         ('metrics', []),
         ('properties', {})
     ])
-    
+
     def __init__(self):
         """Initializes a new instance of the class.
         """
@@ -23,48 +27,49 @@ class MetricData(object):
             'ver': 2,
             'metrics': [],
         }
+        self._context = TelemetryContext()
         self._initialize()
-        
+
     @property
     def ver(self):
         """The ver property.
-        
+
         Returns:
             (int). the property value. (defaults to: 2)
         """
         return self._values['ver']
-        
+
     @ver.setter
     def ver(self, value):
         """The ver property.
-        
+
         Args:
             value (int). the property value.
         """
         self._values['ver'] = value
-        
+
     @property
     def metrics(self):
         """The metrics property.
-        
+
         Returns:
             (list). the property value. (defaults to: [])
         """
         return self._values['metrics']
-        
+
     @metrics.setter
     def metrics(self, value):
         """The metrics property.
-        
+
         Args:
             value (list). the property value.
         """
         self._values['metrics'] = value
-        
+
     @property
     def properties(self):
         """The properties property.
-        
+
         Returns:
             (hash). the property value. (defaults to: {})
         """
@@ -72,11 +77,11 @@ class MetricData(object):
             return self._values['properties']
         self._values['properties'] = copy.deepcopy(self._defaults['properties'])
         return self._values['properties']
-        
+
     @properties.setter
     def properties(self, value):
         """The properties property.
-        
+
         Args:
             value (hash). the property value.
         """
@@ -84,15 +89,24 @@ class MetricData(object):
             del self._values['properties']
         else:
             self._values['properties'] = value
-        
+
+    @property
+    def context(self):
+        """The context property.
+
+        Returns:
+            (object). the property value.
+        """
+        return self._context
+
     def _initialize(self):
         """Initializes the current instance of the object.
         """
         pass
-    
+
     def write(self):
         """Writes the contents of this object and returns the content as a dict object.
-        
+
         Returns:
             (dict). the object that represents the same data as the current instance.
         """
